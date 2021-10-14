46d6f98d3fe7fbf8ca2f113897200c67e8809369
RamonaO <RamonaO@OLTEANUR-PC>
2021-10-14 18:14:16 +0300

    proiect

---
+++ ProiectMaster.Web/SessionHelper2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectMaster.Web
+{
+    public static class SessionHelper2
+    {
+       
+        public const string WishList = "Wishlist";
+
+        public static void Set<T>(this ISession session, string key, T value, int ok)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T Get<T>(this ISession session, string key, int ok)
+        {
+            var value = session.GetString(key);
+
+            return value == null ? default(T) :
+                JsonConvert.DeserializeObject<T>(value);
+        }
+
+
+    }
+}
